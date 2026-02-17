322457beedab45cef4fe5e7c96b21d1a27276b19
SandraPro <xandrapazospro@gmail.com>
2026-02-17 18:00:58 +0100

    adding itunes controller and routes

--- src/controllers/itunes.controller.js
+++ src/controllers/itunes.controller.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-exports.seachSongs = async (req, res) => {
+exports.searchSongs = async (req, res) => {
     try {
         const { term } = req.query;
         const response = await axios.get("https://itunes.apple.com/search",

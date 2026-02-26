5f37a9c53c1f844eebca11bccae2c7793506d394
SandraPro <xandrapazospro@gmail.com>
2026-02-26 19:15:36 +0100

    protecting middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,30 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../utils/catchAsync');
 
-const auth = (req, res, next) => {
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access.', 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const currentUser = await User.findById(decoded.id);  
+
+  if (!currentUser) {
+    return next(new AppError('The user belonging to this token does no longer exist.', 401));
+  }
+
+  req.user = currentUser; // Attach user info to request object
+  next();
+});
+
+/* const auth = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1]; // Extract token from Authorization header
   if (!token) {
     return res.status(401).json({ message: 'No token provided, access denied' });
@@ -15,4 +39,4 @@ const auth = (req, res, next) => {
   }
 };
 
-module.exports = auth;
+module.exports = auth; */
--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
+const { protect } = require("../middlewares/auth.middleware");
+const { createSong} = require("../controllers/song.controller");
+
+router.post("/", protect, createSong);
+module.exports = router;
+
+/*
 const auth = require("../middlewares/auth.middleware");
 const {getSongs, createSong, updateSong, deleteSong} = require("../controllers/song.controller");
 
 router.get("/", auth, getSongs);
 router.post("/", auth, createSong);
 router.put("/:id", auth, updateSong);
-router.delete("/:id", auth, deleteSong);
+router.delete("/:id", auth, deleteSong);*/
 
-module.exports = router;

74e8f10f030e66ca0f9c68b94a3d8e0fcbcd2482
SandraPro <xandrapazospro@gmail.com>
2026-02-28 12:45:47 +0100

    fixed put,get and create song

--- src/controllers/song.controller.js
+++ src/controllers/song.controller.js
@@ -1,26 +1,27 @@
 const Song = require('../models/Song');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getSongs = async (req, res) => {
-    const songs = await Song.find( {user: req.user} );
-    res.json(songs);            
-}
+exports.getSongs = catchAsync(async (req, res) => {
+    const songs = await Song.find({user: req.user});
+    res.json(songs);
+});
 
-exports.createSong = async (req, res) => {
+exports.createSong = catchAsync(async (req, res) => {
     const newSong = await Song.create({...req.body, user: req.user});
     res.json(newSong);
-}
+});
 
-exports.updateSong = async (req, res) => {
+exports.updateSong = catchAsync(async (req, res) => {
     const updatedSong = await Song.findOneAndUpdate(
         {_id: req.params.id, user: req.user},
         req.body,
         {new: true}
     );
     res.json(updatedSong);
-}
+});
 
-exports.deleteSong = async (req, res) => {
+exports.deleteSong = catchAsync(async (req, res) => {
     await Song.findOneAndDelete({_id: req.params.id, user: req.user});
     res.json({message: 'Song deleted'});
-}   
+});   
     
--- src/routes/song.routes.js
+++ src/routes/song.routes.js
@@ -2,17 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require("../middlewares/auth.middleware");
-const { createSong} = require("../controllers/song.controller");
-
-router.post("/", protect, createSong);
-module.exports = router;
-
-/*
-const auth = require("../middlewares/auth.middleware");
 const {getSongs, createSong, updateSong, deleteSong} = require("../controllers/song.controller");
 
-router.get("/", auth, getSongs);
-router.post("/", auth, createSong);
-router.put("/:id", auth, updateSong);
-router.delete("/:id", auth, deleteSong);*/
+router.get("/", protect, getSongs);
+router.post("/", protect, createSong);
+router.put("/:id", protect, updateSong);
+router.delete("/:id", protect, deleteSong);
+
+module.exports = router;
 

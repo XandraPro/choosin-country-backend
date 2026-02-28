f7e1e8f8f694e6b16e16e93cdd8d577a4f096166
SandraPro <xandrapazospro@gmail.com>
2026-02-28 13:05:59 +0100

    render crud fixed

--- src/controllers/song.controller.js
+++ src/controllers/song.controller.js
@@ -2,18 +2,18 @@ const Song = require('../models/Song');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getSongs = catchAsync(async (req, res) => {
-    const songs = await Song.find({user: req.user});
+    const songs = await Song.find({user: req.user._id});
     res.json(songs);
 });
 
 exports.createSong = catchAsync(async (req, res) => {
-    const newSong = await Song.create({...req.body, user: req.user});
+    const newSong = await Song.create({...req.body, user: req.user._id});
     res.json(newSong);
 });
 
 exports.updateSong = catchAsync(async (req, res) => {
     const updatedSong = await Song.findOneAndUpdate(
-        {_id: req.params.id, user: req.user},
+        {_id: req.params.id, user: req.user._id},
         req.body,
         {new: true}
     );
@@ -21,7 +21,7 @@ exports.updateSong = catchAsync(async (req, res) => {
 });
 
 exports.deleteSong = catchAsync(async (req, res) => {
-    await Song.findOneAndDelete({_id: req.params.id, user: req.user});
+    await Song.findOneAndDelete({_id: req.params.id, user: req.user._id});
     res.json({message: 'Song deleted'});
 });   
     
